54fce233a099f1162405bec7287cad46b877728c
Manoj Kumar <mu.kumaran1991@gmail.com>
2025-04-22 13:27:05 +0530

    feat:Added main.js file and addednewline in index.html

---
+++ main.js
@@ -0,0 +1,6 @@
+// This is a backend work by backend developers
+function show(){
+    var sname = document.getElementById("sname").value
+    console.log("Your name: ",sname)
+    document.getElementById("studname").innerHTML = "Your name: "+sname
+}
